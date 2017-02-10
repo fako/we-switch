46e596472ab8ca8671fe5561aa1555ef561ffc66
Fako Berkers <email@fakoberkers.nl>
2017-02-10 18:06:09 +0100

    Rename and simplify the app.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,14 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 
-import { TabsPage } from '../pages/tabs/tabs';
+import { AdvertisementPage } from '../pages/advertisement/advertisement';
 
 
 @Component({
   templateUrl: 'app.html'
 })
-export class MyApp {
-  rootPage = TabsPage;
+export class WeSwitchApp {
+  rootPage = AdvertisementPage;
 
   constructor(platform: Platform) {
     platform.ready().then(() => {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,21 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { MyApp } from './app.component';
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
+
+import { WeSwitchApp } from './app.component';
+import { AdvertisementPage } from '../pages/advertisement/advertisement';
 
 @NgModule({
   declarations: [
-    MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
+    WeSwitchApp,
+    AdvertisementPage
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(WeSwitchApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
+    WeSwitchApp,
+    AdvertisementPage
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
